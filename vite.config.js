31123fd7a6765d5f1032196455e2598832263f31
Viktoriia Cherkashyna <vikushacherk98@gmail.com>
2024-02-17 13:49:22 +0200

    update config

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,13 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  alias: {
-    src: '/src',
-    components: '/src/components',
-    pages: '/src/pages',
-    assets: '/src/assets',
+  resolve: {
+    alias: {
+      src: '/src',
+      components: '/src/components',
+      pages: '/src/pages',
+      assets: '/src/assets',
+    },
   },
   base: '/car-adverts/',
 });
