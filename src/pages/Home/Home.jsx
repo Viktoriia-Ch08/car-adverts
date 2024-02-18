35a3e9136df2b18b7407ba8efdd9381ff4d62cb0
Viktoriia Cherkashyna <vikushacherk98@gmail.com>
2024-02-18 22:32:52 +0200

    fixed

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,21 +5,19 @@ const Home = () => {
     <HomeContainer>
       <TextWrap>
         <MainTitle>Welcome to RentalCar website!</MainTitle>
-        <div>
-          <HomeText>
-            RentalCar is a car rental company that allows you to rent a car
-            quickly and cheaply. We offer short-term and long-term car rental.
-            All cars have compulsory civil liability insurance and an additional
-            comprehensive insurance package. In addition, all vehicles are
-            properly prepared for the trip - they have all the necessary fluids
-            and a full tank of fuel.
-          </HomeText>
-          <HomeText>
-            Our clients can choose cars with gasoline and diesel engines, with
-            manual or automatic transmission, so that everyone can choose a
-            vehicle according to their skills.
-          </HomeText>
-        </div>
+        <HomeText>
+          RentalCar is a car rental company that allows you to rent a car
+          quickly and cheaply. We offer short-term and long-term car rental. All
+          cars have compulsory civil liability insurance and an additional
+          comprehensive insurance package. In addition, all vehicles are
+          properly prepared for the trip - they have all the necessary fluids
+          and a full tank of fuel.
+        </HomeText>
+        <HomeText>
+          Our clients can choose cars with gasoline and diesel engines, with
+          manual or automatic transmission, so that everyone can choose a
+          vehicle according to their skills.
+        </HomeText>
       </TextWrap>
     </HomeContainer>
   );
