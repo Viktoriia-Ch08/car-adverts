f32de00f2449eea0ee07de6524fbc78197c1b23f
Viktoriia Cherkashyna <vikushacherk98@gmail.com>
2024-02-18 21:55:41 +0200

    fix rout that doesn`t exist

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ function App() {
         <Route index element={<Home />} />
         <Route path="/catalog" element={<Catalog />} />
         <Route path="/favorites" element={<Favorites />} />
+        <Route path="*" element={<Home />} />
       </Route>
     </Routes>
   );
