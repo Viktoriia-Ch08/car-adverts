24c0c418352d15fea404e429fcf2bb5c18180191
Viktoriia Cherkashyna <vikushacherk98@gmail.com>
2024-02-19 18:03:07 +0200

    small fix

--- src/main.jsx
+++ src/main.jsx
@@ -12,7 +12,7 @@ import { ToastContainer } from 'react-toastify';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/car-adverts">
+      <BrowserRouter basename="/car-adverts/">
         <GlobalStyle />
         <App />
         <ToastContainer />
