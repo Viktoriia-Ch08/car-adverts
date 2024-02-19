b9593805c975669eb75a4614fcf45aa12ac9c37f
Viktoriia Cherkashyna <vikushacherk98@gmail.com>
2024-02-19 16:13:51 +0200

    update icon

--- src/components/CatalogList/CatalogListItem/CatalogListItem.jsx
+++ src/components/CatalogList/CatalogListItem/CatalogListItem.jsx
@@ -10,6 +10,7 @@ import {
   InfoText,
   LearnMoreBtn,
   LoveBtn,
+  LoveBtnIcon,
   Model,
   Price,
   Title,
@@ -54,16 +55,16 @@ const CatalogListItem = ({ advert, favorite, setFavorite }) => {
             onClick={handleFavorite}
             className={isFavorite ? 'favorite' : ''}
           >
-            <svg
+            <LoveBtnIcon
               xmlns="http://www.w3.org/2000/svg"
-              width="18"
-              height="18"
               viewBox="0 0 18 18"
               fill="none"
               className={isFavorite ? 'favorite-icon' : 'heart'}
             >
-              <path d="M4.03553 1C1.80677 1 0 2.80677 0 5.03553C0 6.10582 0.42517 7.13228 1.18198 7.88909L7.14645 13.8536C7.34171 14.0488 7.65829 14.0488 7.85355 13.8536L13.818 7.88909C14.5748 7.13228 15 6.10582 15 5.03553C15 2.80677 13.1932 1 10.9645 1C9.89418 1 8.86772 1.42517 8.11091 2.18198L7.5 2.79289L6.88909 2.18198C6.13228 1.42517 5.10582 1 4.03553 1Z" />
-            </svg>
+              <path
+                d="M13.91,6.75c-1.17,2.25-4.3,5.31-6.07,6.94c-0.1903,0.1718-0.4797,0.1718-0.67,0C5.39,12.06,2.26,9,1.09,6.75&#xA;&#x9;C-1.48,1.8,5-1.5,7.5,3.45C10-1.5,16.48,1.8,13.91,6.75z"
+              />
+            </LoveBtnIcon>
           </LoveBtn>
           <CardImage
             src={imgUrl}
